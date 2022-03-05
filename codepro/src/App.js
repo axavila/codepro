3ac0b791e07e1c310914fe606764eea9611f790a
Axel Fernando Avila Fernandez <ax.avila@hotmail.com>
2022-03-05 07:24:05 -0600

    facerecognition

--- codepro/src/App.js
+++ codepro/src/App.js
@@ -4,6 +4,7 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import './App.css';
 
 
@@ -23,6 +24,54 @@ const particlesOptions = {
   }
 }
 class App extends Component{
+  constructor() {
+    super();
+    this.state = initialState;
+  }
+
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    fetch('https://calm-lowlands-63429.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+          input: this.state.input,
+          
+      })
+
+    })
+      .then(response => response.json())
+      .then(response => {
+        if (response) {
+          fetch('https://calm-lowlands-63429.herokuapp.com/image', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: this.state.user.id,
+                
+            })
+
+          })
+
+            .then(response => response.json())
+            .then(count => {
+              this.setState(Object.assign(this.state.user, {entries: count}))
+            })
+            .catch(console.log)
+        }
+        
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+      .catch(err => console.log(err));
+
+  }
+
+
   render(){
   return (
     <div className="App">
@@ -34,9 +83,7 @@ class App extends Component{
       <Logo />
       <Rank />
       <ImageLinkForm />
-     {/* <Logo />
-      <ImageLinkForm />
-      <FaceRecognition /> */}
+     <FaceRecognition />
       
       
     </div>
